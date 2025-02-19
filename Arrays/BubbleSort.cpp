9856b6ebc73e67eb0f64b77f9022da60bc06d781
Abdelaziz Bahoumi <Abdobm2811@gmail.com>
2025-02-19 21:55:26 +0100

    update bubble sort problem

--- Arrays/BubbleSort.cpp
+++ Arrays/BubbleSort.cpp
@@ -3,14 +3,21 @@
 
 using namespace std;
 
-vector<int> sortArray(vector<int>& nums) {
+void Swap(vector<int>& nums, int i, int j) {
+
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+
+vector<int> SortArray(vector<int>& nums) {
         
     for (short i = 0; i < nums.size(); i++) {
 
-        for (short j = 0; j < nums.size() - i - 1; j++) {
+        for (short j = 0; j < nums.size() - i; j++) {
 
             if (nums[j] > nums[j + 1])
-                swap(nums[j], nums[j + 1]);
+                Swap(nums, nums[j], nums[j + 1]);
         }
     }
 
@@ -19,8 +26,9 @@ vector<int> sortArray(vector<int>& nums) {
 
 int main() {
 
-    vector<int> vnumbers = { 2, 4, 5, 3, 6 };
-    vnumbers = sortArray(vnumbers);
+    vector<int> vnumbers = { 10, 9, 34, 8, 2, 4, 5, 3, 6 };
+    
+    vnumbers = SortArray(vnumbers);
 
     for (int& num : vnumbers)
         cout << num << ' ';
