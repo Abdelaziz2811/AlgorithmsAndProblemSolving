b5c83cac19bf9e22fc7c3dfcf3668d8a3ab88e2b
Abdelaziz Bahoumi <Abdobm2811@gmail.com>
2025-02-21 21:54:26 +0100

    optimize valid anagram problem

--- Arrays/ValidAnagram.cpp
+++ Arrays/ValidAnagram.cpp
@@ -3,37 +3,25 @@
 #include <vector>
 
 using namespace std;
-vector <char> Split(string s)
-{
-    vector <char> result;
-    for (int i = 0; i < s.size(); i++)
-    {
-        result.push_back(s[i]);
-    }
-    return result;
-}
+bool IssLExistIntL(char tL, string& s) {
 
-bool IssLExistIntL(char sL, vector <char>& sLetters)
-{
-    for (auto sLetter = sLetters.begin() ; sLetter != sLetters.end(); sLetter++)
-    {
-        if (sL == *sLetter)
-        {
-            sLetters.erase(sLetter);
+    for (auto sL = s.begin(); sL != s.end(); sL++) {
+        if (tL == *sL) {
+            s.erase(sL);
             return true;
         }
     }
     return false;
-}   
+}
 
 bool IsAnagram(string s, string t) {
-    
-    if (s.length() != t.length()) return false;
 
-    vector <char> sLetters = Split(s);
-    for (int i = 0; i < t.length(); i++)
-    {
-        if (!IssLExistIntL(t[i], sLetters)) return false;
+    if (s.length() != t.length())
+        return false;
+
+    for (int i = 0; i < t.length(); i++) {
+        if (!IssLExistIntL(t[i], s))
+            return false;
     }
     return true;
 }
