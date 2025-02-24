3aa03e1fa98117625acf56d4a7516a60fa015474
Abdelaziz Bahoumi <Abdobm2811@gmail.com>
2025-02-24 18:08:11 +0000

    implement the solution structure

---
+++ Arrays/letterCombination.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<string> letterCombinations(string digits) {
+        
+    const vector<string> keypad {
+        "", "", "abc","def",
+        "ghi","jkl","mno",
+        "pqrs","tuv","wxyz"
+    };
+
+    string letterCombination;
+    vector<string> Combination;
+
+    for (short i = stoi(to_string(digits[0])); i < stoi(to_string(digits[digits.length() - 1])); i++) {
+
+        for (short j = 0; j < keypad[i].length(); j++) {
+
+            if (digits.length() > 1) {
+                
+                Combination.push_back(letterCombination);
+            }
+            else Combination.push_back(j);
+        }
+    }
+
+    return Combination;
+}
+
+int main() {
+
+    return 0;
+}
