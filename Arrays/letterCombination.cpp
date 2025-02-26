a8bb6cce8107e8384599112e36b67d6f17c13c4e
Abdelaziz Bahoumi <Abdobm2811@gmail.com>
2025-02-26 17:33:48 +0000

    solve letter combination problem

--- Arrays/letterCombination.cpp
+++ Arrays/letterCombination.cpp
@@ -1,35 +1,37 @@
-#include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
 vector<string> letterCombinations(string digits) {
-        
+    if (digits.empty()) return {};
+
     const vector<string> keypad {
-        "", "", "abc","def",
-        "ghi","jkl","mno",
-        "pqrs","tuv","wxyz"
+        "", "", "abc", "def",
+        "ghi", "jkl", "mno",
+        "pqrs", "tuv", "wxyz"
     };
 
-    string letterCombination;
-    vector<string> Combination;
+    vector<string> result;
+    result.push_back("");
 
-    for (short i = stoi(to_string(digits[0])); i < stoi(to_string(digits[digits.length() - 1])) - 1; i++) {
+    for (char digit : digits) {
+        int num = digit - '0';
+        if (num < 0 || num > 9) continue;
 
-        if (digits.length() > 1) {
-            
-            for(char C_1 : keypad[i]) {
+        const string& candidates = keypad[num];
+        if (candidates.empty()) continue;
 
-                for (char C_2 : keypad[i + 1]) {
-
-                    letterCombination = C_1 + C_2;
-                    Combination.push_back(letterCombination);
-                }
+        vector<string> temp;
+        for (const string& combination : result) {
+            for (char letter : candidates) {
+                temp.push_back(combination + letter);
             }
         }
+        result.swap(temp);
     }
 
-    return Combination;
+    return result;
 }
 
 int main() {
